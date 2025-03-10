9819d90a597cba97dfa2c994f147cfbfc3d90f76
guojihu-hana <guojihu@pjlab.org.cn>
2025-03-10 22:43:59 +0800

    Delete unused files

--- ExhaustedPP/main.py
+++
@@ -1,9 +0,0 @@
-from abstract.Pipeline import PipelineScheduler
-
-if __name__ == "__main__":
-    pp = PipelineScheduler()
-    pp.run_pipeline_parallelism(time_limit=2000)
-    pp.show_mem_usage()
-    pp.draw()
-    pass
-
